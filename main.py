143a8261488c98358c295befac52c46a3400ec00
Alice1esiee <alice.lin@edu.esiee.fr>
2024-09-13 08:34:07 +0000

    exo nb premier Alice LIN

--- main.py
+++ main.py
@@ -1,21 +1,44 @@
+"""Fonction qui importe le module de la racine carree."""
 from math import sqrt
 
 #### Fonction secondaire
 
-
 def isprime(p):
+    """
 
-    # votre code ici
+    Retourne True ou False si c'est un nombre premier ou non.
 
-    pass
+    >>>isprime(1)
+    False
+    >>>isprime(4)
+    False
+    >>>isprime(7)
+    True
+    >>>isprime(0)
+    False
+
+    """
+    premier = True
+    if p in (0,1):
+        premier = False
+    for i in range(2,p):
+        if p%i == 0 or sqrt(p)%i == 0:
+            premier = False
+    return premier
 
 #### Fonction principale
 
 
 def main():
+    """
 
-    # vos appels à la fonction secondaire ici
+    Retourne tous les nombres premiers a partir de la fonction isprime qui determine 
+    s'il est premier ou non.
 
+    >>>main()
+    2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 
+
+    """
     for n in range(100):
         if isprime(n):
             print(n, end=", ")
